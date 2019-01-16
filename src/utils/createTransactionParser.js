79af7a693f87b813896968fbb5a8ed08a184240c
Pablo Enrici <pablo@bloq.com>
2019-01-16 18:24:01 -0300

    Add export-related fields to transaction parser

--- src/utils/createTransactionParser.js
+++ src/utils/createTransactionParser.js
@@ -1,4 +1,5 @@
 import BigNumber from 'bignumber.js'
+import moment from 'moment'
 import get from 'lodash/get'
 
 function isAuctionTransaction(rawTx) {
@@ -61,7 +62,11 @@ function getTo(rawTx, tokenData, txType) {
 function getValue(rawTx, tokenData, txType) {
   return ['received', 'sent'].includes(txType) && tokenData && tokenData.value
     ? tokenData.value
-    : rawTx.transaction.value
+    : txType === 'exported'
+      ? get(rawTx, ['meta', 'metronome', 'export', 'value'], null)
+      : txType === 'imported'
+        ? get(rawTx, ['meta', 'metronome', 'import', 'value'], null)
+        : rawTx.transaction.value
 }
 
 function getCoinSpentInAuction(rawTx, txType) {
@@ -154,34 +159,65 @@ function getBlockNumber(rawTx) {
 
 // TODO: implement!
 function getImportedFrom() {
-  return 'ETC'
+  return 'ORIGIN CHAIN'
 }
 
-// TODO: implement!
-function getExportedTo() {
-  return 'ETH'
+function getExportedTo(rawTx) {
+  return get(rawTx, ['meta', 'metronome', 'export', 'destinationChain'], '')
+}
+
+function getPortFee(rawTx) {
+  return get(rawTx, ['meta', 'metronome', 'export', 'fee'], null)
+}
+
+function getPortDestinationAddress(rawTx) {
+  return get(rawTx, ['meta', 'metronome', 'export', 'to'], null)
+}
+
+function getPortBurnHash(rawTx) {
+  return get(rawTx, ['meta', 'metronome', 'export', 'currentBurnHash'], null)
+}
+
+// TODO: in the future other transaction types will include a timestamp
+function getTimestamp(rawTx) {
+  const timestamp = get(
+    rawTx,
+    ['meta', 'metronome', 'export', 'blockTimestamp'],
+    null
+  )
+  return timestamp ? Number(timestamp) : null
+}
+
+function getFormattedTime(timestamp) {
+  return timestamp ? moment.unix(timestamp).format('LLLL') : null
 }
 
 export const createTransactionParser = myAddress => rawTx => {
   const tokenData = Object.values(rawTx.meta.tokens || {})[0] || null
   const txType = getTxType(rawTx, tokenData, myAddress)
   const convertedFrom = getConvertedFrom(rawTx, txType)
+  const timestamp = getTimestamp(rawTx, txType)
 
   return {
+    portDestinationAddress: getPortDestinationAddress(rawTx),
     metBoughtInAuction: getMetBoughtInAuction(rawTx, tokenData, txType),
     contractCallFailed: getContractCallFailed(rawTx),
     coinSpentInAuction: getCoinSpentInAuction(rawTx, txType),
     isCancelApproval: getIsCancelApproval(tokenData),
     approvedValue: getApprovedValue(tokenData),
+    formattedTime: getFormattedTime(timestamp),
     convertedFrom,
     isProcessing: getIsProcessing(tokenData),
+    portBurnHash: getPortBurnHash(rawTx),
     importedFrom: getImportedFrom(),
     blockNumber: getBlockNumber(rawTx),
     isApproval: getIsApproval(tokenData),
-    exportedTo: getExportedTo(),
+    exportedTo: getExportedTo(rawTx),
     fromValue: getFromValue(rawTx, tokenData, convertedFrom),
+    timestamp,
     toValue: getToValue(rawTx, tokenData, convertedFrom),
     gasUsed: getGasUsed(rawTx),
+    portFee: getPortFee(rawTx),
     txType,
     symbol: getSymbol(tokenData, txType),
     value: getValue(rawTx, tokenData, txType),
