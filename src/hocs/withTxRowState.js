c645644db845ecb4d03d08f2bf7238128642db41
Pablo Enrici <pablo@bloq.com>
2019-03-18 18:04:48 -0300

    Cleanup unneeded props

--- src/hocs/withTxRowState.js
+++ src/hocs/withTxRowState.js
@@ -7,10 +7,6 @@ import React from 'react'
 const withTxRowState = WrappedComponent => {
   class Container extends React.Component {
     static propTypes = {
-      activeChainConfig: PropTypes.shape({
-        converterAddress: PropTypes.string.isRequired,
-        metTokenAddress: PropTypes.string.isRequired
-      }).isRequired,
       confirmations: PropTypes.number.isRequired,
       coinSymbol: PropTypes.string.isRequired,
       tx: PropTypes.shape({
@@ -24,12 +20,10 @@ const withTxRowState = WrappedComponent => {
       WrappedComponent.name})`
 
     render() {
-      const { tx, confirmations, activeChainConfig } = this.props
+      const { tx, confirmations } = this.props
 
       return (
         <WrappedComponent
-          converterAddress={activeChainConfig.converterAddress}
-          metTokenAddress={activeChainConfig.metTokenAddress}
           isPending={utils.isPending(tx, confirmations)}
           isFailed={utils.isFailed(tx, confirmations)}
           {...this.props}
@@ -41,7 +35,6 @@ const withTxRowState = WrappedComponent => {
 
   const mapStateToProps = (state, props) => ({
     // avoid unnecessary re-renders once transaction is confirmed
-    activeChainConfig: selectors.getActiveChainConfig(state),
     confirmations: Math.min(6, selectors.getTxConfirmations(state, props)),
     coinSymbol: selectors.getCoinSymbol(state)
   })
